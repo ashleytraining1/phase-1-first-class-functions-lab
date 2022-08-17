99e3c2412c9953d8f50b24d62b929840623dcfd0
ashley <ashley.oketo@student.moringaschool.com>
2022-08-17 22:27:10 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function (drivers) {
+    
+    const firstTwoDrivers = drivers.slice(0,2)
+
+    console.log(firstTwoDrivers);
+
+    return firstTwoDrivers;
+}
+const returnLastTwoDrivers = function (drivers) {
+
+    const lastTwoDrivers = drivers.slice(2,4)
+
+    console.log(lastTwoDrivers);
+
+    return lastTwoDrivers;
+
+}
+
+let selectingDrivers = []
+
+selectingDrivers.push(returnFirstTwoDrivers);
+selectingDrivers.push(returnLastTwoDrivers);
+
+const createFareMultiplier = (number) => {
+
+    return function multiplier (fare) {
+
+        return fare * number
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = (drivers,returnFirstTwoDrivers) => {
+    return returnFirstTwoDrivers(drivers);
+}
